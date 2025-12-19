5eef521af20bb6630ecafcd0ddaddbfdb0b9de2f
SandeshJadhav7320 <sandeshj9995@gmail.com>
2025-12-19 11:10:14 +0530

    updated backend

---
+++ backend/backend/src/main/java/com/example/backend/Common/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.example.backend.Common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        // ✅ Allow requests from ANY PC (DEV)
+        config.addAllowedOrigin("*");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
