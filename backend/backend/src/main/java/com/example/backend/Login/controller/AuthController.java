8da33af75aff526fdce4278e4842f5e17eb4a211
SandeshJadhav7320 <sandeshj9995@gmail.com>
2025-12-19 10:47:49 +0530

    cross origin changed

--- backend/backend/src/main/java/com/example/backend/Login/controller/AuthController.java
+++ backend/backend/src/main/java/com/example/backend/Login/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController 
